a66268bb7db41ae70d0c5d726419025fe5392e05
Spencer Ng <sng1488@gmail.com>
2021-10-18 11:32:01 -0500

    Add skeleton code for mobile sensing app

---
+++ hw3/app/src/main/java/com/sng/cs23218/racingthroughlife/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sng.cs23218.racingthroughlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
